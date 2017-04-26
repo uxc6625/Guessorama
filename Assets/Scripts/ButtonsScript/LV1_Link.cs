d961aa64c7b043087508ac20f4cbb2ca8a9350ca
DESKTOP-N44B0D5\ulijah <uxc6625@g.rit.edu>
2017-04-25 20:46:58 -0400

    Linked all the levels Added a text notifications

---
+++ Assets/Scripts/ButtonsScript/LV1_Link.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LV1_Link : MonoBehaviour {
+
+    public void OnStartGame()
+    {
+        Debug.Log("You pressed start game!");
+        Application.LoadLevel("LV1");
+    }
+}
---
+++ Assets/Scripts/ButtonsScript/LV3_Link.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LV3_Link : MonoBehaviour {
+
+    public void OnStartGame()
+    {
+        Debug.Log("You pressed start game!");
+        Application.LoadLevel("LV3");
+    }
+}
