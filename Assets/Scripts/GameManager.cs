b665460b9d806f88c86fef380d9cac59f1d4001f
DESKTOP-N44B0D5\ulijah <uxc6625@g.rit.edu>
2017-04-28 13:24:20 -0400

    UI_V1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,12 +8,20 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour {
 
 	public Prompt[] prompts;
+    public GameObject Notification;
 	private static List<Prompt> remainingPrompts;
 
-	private Prompt currentPrompt;
+    //public Vector2 startinPos, endPos, size;
+    public static Vector2 startingPosition;
+    public static Vector2 wantedPosition;
+    private static Vector3 velocity3 = Vector3.zero;
+    private Prompt currentPrompt;
+    //private float speed = 1.1f;
 
-	// Stores the value of the prompt
-	[SerializeField]
+
+
+    // Stores the value of the prompt
+    [SerializeField]
 	private Text promptText;
 
 	// Stores the value of the input
@@ -33,7 +41,10 @@ public class GameManager : MonoBehaviour {
 		}
 
 		SetCurrentPrompt();
-	}
+        Notification.SetActive(false);
+        startingPosition = Notification.transform.position;
+        wantedPosition = new Vector2 (Notification.transform.position.x,Notification.transform.position.y + 100);
+    }
 	
 	// Update is called once per frame
 	void Update () { }
@@ -64,7 +75,25 @@ public class GameManager : MonoBehaviour {
 		
 		// Correct answer (needs work) - This could also be index >= 0
 		if (currentPrompt.answers.Contains(inputText.text)) {
-			Debug.Log("Correct answer!" + inputText.text);
+
+
+            //float step = speed * Time.deltaTime;
+            Notification.SetActive(true);
+            //Notification.transform.position = new Vector2(Notification.transform.position.x,Notification.transform.position.y + 100);
+            Notification.transform.position = Vector2.Lerp(Notification.transform.position, wantedPosition,100);
+            //Notification.transform.position = Vector3.SmoothDamp(startingPosition,wantedPosition, ref velocity, 0.3F);
+            //Notification.transform.position = Vector3.MoveTowards(wantedPosition, startingPosition, step);
+            //Camera.main.transform.position = new Vector3 (Camera.main.transform.position.x + 50,Camera.main.transform.position.y);
+
+            //Notification.transform.position = Vector3.SmoothDamp(wantedPosition, startingPosition, ref velocity3, 2);
+            StartCoroutine(Timer());
+            
+
+
+            //Notification.transform.position = Vector3.SmoothDamp(startingPosition, startingPosition, ref velocity3, 0.35f);
+            //Notification.transform.position = Vector3.SmoothDamp(startingPosition, wantedPosition, ref velocity3, 0.35f);
+
+            Debug.Log("Correct answer!" + inputText.text);
 			// Set the color of the text in the box black	
 			termsText[index].color = Color.black;
 		} else {
@@ -72,4 +101,13 @@ public class GameManager : MonoBehaviour {
 			Debug.Log("User input: " + inputText.text);
 		}
 	}
+
+    IEnumerator Timer()
+    {
+        
+        yield return new WaitForSecondsRealtime(3);
+
+        Notification.SetActive(false);
+        Notification.transform.position = new Vector2(Notification.transform.position.x, Notification.transform.position.y - 100);
+    }
 }
